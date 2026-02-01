c416edbbd84f978b69c16e03a4cd79b8df736c4d
Eduardo Santos <eduardocod@yahoo.com.br>
2026-02-01 15:48:54 -0300

    Desafio 1: Sobrevivendo sem Data Test ID

---
+++ playwright/e2e/pedidos.spec.eduardorsnts.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-QYESMH');
+  await page.locator('//button[text()="Buscar Pedido"]').click();
+
+  await expect(page.getByText('Pedido', { exact: true })).toBeVisible({timeout: 30_000});
+  await expect(page.getByText('VLO-QYESMH')).toBeVisible();
+  await expect(page.getByText('APROVADO')).toBeVisible();
+});
